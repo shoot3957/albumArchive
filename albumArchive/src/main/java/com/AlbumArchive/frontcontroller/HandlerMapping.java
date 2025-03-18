7c7ff933b7d07642c62fd72f9a95abf7a8bd2aaa
psh1008 <pshpsh1008@gmail.com>
2025-03-18 09:59:58 +0900

    .
    .

--- albumArchive/src/main/java/com/AlbumArchive/frontcontroller/HandlerMapping.java
+++ albumArchive/src/main/java/com/AlbumArchive/frontcontroller/HandlerMapping.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import com.AlbumArchive.controller.MainController;
 import com.AlbumArchive.controller.album.AlbumListAllController;
 import com.AlbumArchive.controller.album.AlbumSortedByGenre;
+import com.AlbumArchive.controller.member.FindIdController;
 import com.AlbumArchive.controller.member.JoinController;
 import com.AlbumArchive.controller.member.LoginController;
 import com.AlbumArchive.controller.member.LogoutController;
@@ -23,10 +24,13 @@ public class HandlerMapping {
 		mappings.put("/login.do", new LoginController());
 		mappings.put("/vaildIdAjax.do", new VaildIdAjaxController());
 		mappings.put("/logout.do", new LogoutController());
+		mappings.put("/findId.do", new FindIdController());
+
 		
 		mappings.put("/albumListAll.do", new AlbumListAllController());
         mappings.put("/albumSortedByGenre.do", new AlbumSortedByGenre());  // 장르별 앨범 리스트
-		
+        
+        
 	}
 
 	public Controller getController(String key) {
