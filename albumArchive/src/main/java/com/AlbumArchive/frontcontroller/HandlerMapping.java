e0017f788e461045d198991e1a3f1cd0aa3e12d7
psh1008 <pshpsh1008@gmail.com>
2025-03-18 10:05:26 +0900

    .
    .

--- albumArchive/src/main/java/com/AlbumArchive/frontcontroller/HandlerMapping.java
+++ albumArchive/src/main/java/com/AlbumArchive/frontcontroller/HandlerMapping.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 
 import com.AlbumArchive.controller.MainController;
 import com.AlbumArchive.controller.album.AlbumListAllController;
+import com.AlbumArchive.controller.album.AlbumSortedByArtist;
 import com.AlbumArchive.controller.album.AlbumSortedByGenre;
-import com.AlbumArchive.controller.member.FindIdController;
+import com.AlbumArchive.controller.album.AlbumSortedByLikes;
 import com.AlbumArchive.controller.member.JoinController;
 import com.AlbumArchive.controller.member.LoginController;
 import com.AlbumArchive.controller.member.LogoutController;
@@ -14,26 +15,24 @@ import com.AlbumArchive.controller.member.VaildIdAjaxController;
 
 
 public class HandlerMapping {
-	private HashMap<String, Controller> mappings;
+    private HashMap<String, Controller> mappings;
 
-	public HandlerMapping() {
-		mappings = new HashMap<String, Controller>();
+    public HandlerMapping() {
+        mappings = new HashMap<String, Controller>();
 
-		mappings.put("/main.do", new MainController());
-		mappings.put("/join.do", new JoinController());
-		mappings.put("/login.do", new LoginController());
-		mappings.put("/vaildIdAjax.do", new VaildIdAjaxController());
-		mappings.put("/logout.do", new LogoutController());
-		mappings.put("/findId.do", new FindIdController());
+        mappings.put("/main.do", new MainController());
+        mappings.put("/join.do", new JoinController());
+        mappings.put("/login.do", new LoginController());
+        mappings.put("/vaildIdAjax.do", new VaildIdAjaxController());
+        mappings.put("/logout.do", new LogoutController());
 
-		
-		mappings.put("/albumListAll.do", new AlbumListAllController());
+        mappings.put("/albumListAll.do", new AlbumListAllController());
         mappings.put("/albumSortedByGenre.do", new AlbumSortedByGenre());  // 장르별 앨범 리스트
-        
-        
-	}
+        mappings.put("/albumSortedByLikes.do", new AlbumSortedByLikes());
+        mappings.put("/albumSortedByArtist.do", new AlbumSortedByArtist());
+    }
 
-	public Controller getController(String key) {
-		return mappings.get(key);
-	}
+    public Controller getController(String key) {
+        return mappings.get(key);
+    }
 }
