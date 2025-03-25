cd7bce62a2cb8e515a9794c6a02d1524be7b7bd5
WOWOW0wOw <jongseok8529@gmail.com>
2025-03-25 15:52:46 +0900

    hjs
    hjs

--- albumArchive/src/main/java/com/AlbumArchive/DAO/LikesDAO.java
+++ albumArchive/src/main/java/com/AlbumArchive/DAO/LikesDAO.java
@@ -1,5 +1,71 @@
 package com.AlbumArchive.DAO;
 
-public class LikesDAO {
+import com.AlbumArchive.util.MybatisConfig;
+import org.apache.ibatis.session.SqlSession;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class LikesDAO {
+    private static LikesDAO instance;
+
+    private LikesDAO() {}
+
+    public static LikesDAO getInstance() {
+        if (instance == null) {
+            instance = new LikesDAO();
+        }
+        return instance;
+    }
+
+    public boolean checkLike(String userId, int albumNum) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("userId", userId);
+        params.put("albumNum", albumNum);
+        try (SqlSession session = MybatisConfig.getSqlSessionFactory().openSession()) {
+            int count = session.selectOne("com.AlbumArchive.mybatis.LikesMapper.checkLike", params);
+            return count > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void addLike(String userId, int albumNum) {
+        try (SqlSession session = MybatisConfig.getSqlSessionFactory().openSession()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("userId", userId);
+            params.put("albumNum", albumNum);
+            session.insert("com.AlbumArchive.mybatis.LikesMapper.addLike", params);
+            session.update("com.AlbumArchive.mybatis.LikesMapper.incrementLikes", albumNum);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to add like", e);
+        }
+    }
+
+    public void removeLike(String userId, int albumNum) {
+        try (SqlSession session = MybatisConfig.getSqlSessionFactory().openSession()) {
+            Map<String, Object> params = new HashMap<>();
+            params.put("userId", userId);
+            params.put("albumNum", albumNum);
+            session.delete("com.AlbumArchive.mybatis.LikesMapper.removeLike", params);
+            session.update("com.AlbumArchive.mybatis.LikesMapper.decrementLikes", albumNum);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to remove like", e);
+        }
+    }
+
+    public int getLikesCount(int albumNum) {
+        try (SqlSession session = MybatisConfig.getSqlSessionFactory().openSession()) {
+            Integer count = session.selectOne("com.AlbumArchive.mybatis.LikesMapper.getLikesCount", albumNum);
+            return count != null ? count : 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to get likes count", e);
+        }
+    }
 }
--- albumArchive/src/main/java/com/AlbumArchive/VO/AlbumVO.java
+++ albumArchive/src/main/java/com/AlbumArchive/VO/AlbumVO.java
@@ -1,111 +1,42 @@
 package com.AlbumArchive.VO;
 
 public class AlbumVO {
-	private int num; // 앨범 번호 (AUTO_INCREMENT)
-	private String name; // 앨범명
-	private int artistNum; // 아티스트 번호
-	private String info; // 앨범 설명
-	private String img; // 앨범 이미지 경로
-	private int price; // 가격
-	private int likes; // 좋아요 수
-	private int totalQty; // 총 수량
-	private String category; // 카테고리
-	private String dates; // 등록일
+    private int num;
+    private String name;
+    private int artistNum;
+    private String info;
+    private int price;
+    private int likes;
+    private int totalQty;
+    private String category;
+    private String dates;
+    private String img;
 
-	public AlbumVO() {
-	}
+    public AlbumVO() {}
 
-	public AlbumVO(int num, String name, int artistNum, String info, String img, int price, int likes, int totalQty,
-			String category, String dates) {
-		this.num = num;
-		this.name = name;
-		this.artistNum = artistNum;
-		this.info = info;
-		this.img = img;
-		this.price = price;
-		this.likes = likes;
-		this.totalQty = totalQty;
-		this.category = category;
-		this.dates = dates;
-	}
+    public int getNum() { return num; }
+    public void setNum(int num) { this.num = num; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getArtistNum() { return artistNum; }
+    public void setArtistNum(int artistNum) { this.artistNum = artistNum; }
+    public String getInfo() { return info; }
+    public void setInfo(String info) { this.info = info; }
+    public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
+    public int getLikes() { return likes; }
+    public void setLikes(int likes) { this.likes = likes; }
+    public int getTotalQty() { return totalQty; }
+    public void setTotalQty(int totalQty) { this.totalQty = totalQty; }
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+    public String getDates() { return dates; }
+    public void setDates(String dates) { this.dates = dates; }
+    public String getImg() { return img; }
+    public void setImg(String img) { this.img = img; }
 
-	public int getNum() {
-		return num;
-	}
-
-	public void setNum(int num) {
-		this.num = num;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getArtistNum() {
-		return artistNum;
-	}
-
-	public void setArtistNum(int artistNum) {
-		this.artistNum = artistNum;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
-	}
-
-	public String getImg() {
-		return img;
-	}
-
-	public void setImg(String img) {
-		this.img = img;
-	}
-
-	public int getPrice() {
-		return price;
-	}
-
-	public void setPrice(int price) {
-		this.price = price;
-	}
-
-	public int getLikes() {
-		return likes;
-	}
-
-	public void setLikes(int likes) {
-		this.likes = likes;
-	}
-
-	public int getTotalQty() {
-		return totalQty;
-	}
-
-	public void setTotalQty(int totalQty) {
-		this.totalQty = totalQty;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getDates() {
-		return dates;
-	}
-
-	public void setDates(String dates) {
-		this.dates = dates;
-	}
+    @Override
+    public String toString() {
+        return "AlbumVO [num=" + num + ", name=" + name + ", artistNum=" + artistNum + "]";
+    }
 }
